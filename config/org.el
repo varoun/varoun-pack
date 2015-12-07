eb7f5223e6976cb8041026dbd764b3b1a738af47
Varoun P <contact@varoun.com>
2015-12-07 15:07:29 +0530

    Start setting up org-mode for GTD.

--- config/org.el
+++ config/org.el
@@ -1,12 +1,28 @@
 ;;; Org Mode stuff
 
-(setq org-log-done 'note)
+;;; Directory for all GTD related files.
+(setq org-directory "~/gtd/")
 
+
+
+;;; Global key bindings.
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
 
+;;; For Capture mode.
+(global-set-key "\C-cc" 'org-capture)
+(setq org-default-notes-file (concat org-directory "capture.org"))
 
-(setq org-agenda-files (list "~/notes/work.org"
-                             "~/notes/home.org"))
+
+;;; Agenda files.
+(setq org-agenda-files (list "~/gtd/endurance.org"
+                             "~/gtd/home.org"
+                             "~/gtd/birthdays.org"))
+
+;;; Refile.
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+;;; Todo completions.
+(setq org-log-done 'note)
